a6df571870180621e0d86ba662916a257ff3bd19
vedivero <vedivero@github.com>
2025-03-10 21:31:14 +0900

    feat/product - 상품 등록 기능 추가 및 이미지 업로드 구현

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,59 +1,83 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import api from "../../utils/api";
-import { showToastMessage } from "../common/uiSlice";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import api from '../../utils/api';
+import { showToastMessage } from '../common/uiSlice';
 
 // 비동기 액션 생성
 export const getProductList = createAsyncThunk(
-  "products/getProductList",
-  async (query, { rejectWithValue }) => {}
+   'products/getProductList',
+   async (query, { rejectWithValue }) => {},
 );
 
 export const getProductDetail = createAsyncThunk(
-  "products/getProductDetail",
-  async (id, { rejectWithValue }) => {}
+   'products/getProductDetail',
+   async (id, { rejectWithValue }) => {},
 );
 
 export const createProduct = createAsyncThunk(
-  "products/createProduct",
-  async (formData, { dispatch, rejectWithValue }) => {}
+   'products/createProduct',
+   async (formData, { dispatch, rejectWithValue }) => {
+      try {
+         const response = await api.post('product/regist', formData);
+         if (response.status !== 200) throw new Error(response.error);
+         dispatch(showToastMessage({ message: '상품 등록이 완료되었습니다.', status: 'success' }));
+         return response.data.data;
+      } catch (error) {
+         dispatch(showToastMessage({ message: '상품 등록이 실패했습니다.', status: 'error' }));
+         console.log(error.error);
+         return rejectWithValue(error.error);
+      }
+   },
 );
 
 export const deleteProduct = createAsyncThunk(
-  "products/deleteProduct",
-  async (id, { dispatch, rejectWithValue }) => {}
+   'products/deleteProduct',
+   async (id, { dispatch, rejectWithValue }) => {},
 );
 
 export const editProduct = createAsyncThunk(
-  "products/editProduct",
-  async ({ id, ...formData }, { dispatch, rejectWithValue }) => {}
+   'products/editProduct',
+   async ({ id, ...formData }, { dispatch, rejectWithValue }) => {},
 );
 
 // 슬라이스 생성
 const productSlice = createSlice({
-  name: "products",
-  initialState: {
-    productList: [],
-    selectedProduct: null,
-    loading: false,
-    error: "",
-    totalPageNum: 1,
-    success: false,
-  },
-  reducers: {
-    setSelectedProduct: (state, action) => {
-      state.selectedProduct = action.payload;
-    },
-    setFilteredList: (state, action) => {
-      state.filteredList = action.payload;
-    },
-    clearError: (state) => {
-      state.error = "";
-      state.success = false;
-    },
-  },
-  extraReducers: (builder) => {},
+   name: 'products',
+   initialState: {
+      productList: [],
+      selectedProduct: null,
+      loading: false,
+      error: '',
+      totalPageNum: 1,
+      success: false,
+   },
+   reducers: {
+      setSelectedProduct: (state, action) => {
+         state.selectedProduct = action.payload;
+      },
+      setFilteredList: (state, action) => {
+         state.filteredList = action.payload;
+      },
+      clearError: (state) => {
+         state.error = '';
+         state.success = false;
+      },
+   },
+   extraReducers: (builder) => {
+      builder.addCase(createProduct.pending, (state, action) => {
+         state.loading = true;
+      });
+      builder.addCase(createProduct.fulfilled, (state, action) => {
+         state.loading = false;
+         state.error = '';
+         state.success = true;
+      });
+      builder.addCase(createProduct.rejected, (state, action) => {
+         state.loading = false;
+         state.error = action.payload;
+         state.success = false;
+      });
+   },
 });
 
-export const { setSelectedProduct, setFilteredList, clearError } =
-  productSlice.actions;
+export const { setSelectedProduct, setFilteredList, clearError } = productSlice.actions;
 export default productSlice.reducer;
