0aa5f9cb76bff06a3c40ab90e91fae975a246d69
vedivero <vedivero@github.com>
2025-03-13 12:23:27 +0900

    feat/order - 결제 기능 구현

--- src/features/order/orderSlice.js
+++ src/features/order/orderSlice.js
@@ -1,49 +1,71 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { getCartQty } from "../cart/cartSlice";
-import api from "../../utils/api";
-import { showToastMessage } from "../common/uiSlice";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getCartQty } from '../cart/cartSlice';
+import api from '../../utils/api';
+import { showToastMessage } from '../common/uiSlice';
 
 // Define initial state
 const initialState = {
-  orderList: [],
-  orderNum: "",
-  selectedOrder: {},
-  error: "",
-  loading: false,
-  totalPageNum: 1,
+   orderList: [],
+   orderNum: '',
+   selectedOrder: {},
+   error: '',
+   loading: false,
+   totalPageNum: 1,
 };
 
 // Async thunks
 export const createOrder = createAsyncThunk(
-  "order/createOrder",
-  async (payload, { dispatch, rejectWithValue }) => {}
+   'order/createOrder',
+   async (payload, { dispatch, rejectWithValue }) => {
+      try {
+         const response = await api.post('/order', payload);
+         if (response.status !== 200) throw new Error(response.error);
+         dispatch(showToastMessage({ message: '결제가 완료되었습니다.', status: 'success' }));
+         dispatch(getCartQty());
+         return response.data.orderNum;
+      } catch (error) {
+         console.error(error);
+         dispatch(showToastMessage({ message: '주문 요청이 실패했습니다.', status: 'error' }));
+         return rejectWithValue(error.error);
+      }
+   },
 );
 
-export const getOrder = createAsyncThunk(
-  "order/getOrder",
-  async (_, { rejectWithValue, dispatch }) => {}
-);
+export const getOrder = createAsyncThunk('order/getOrder', async (_, { rejectWithValue, dispatch }) => {});
 
 export const getOrderList = createAsyncThunk(
-  "order/getOrderList",
-  async (query, { rejectWithValue, dispatch }) => {}
+   'order/getOrderList',
+   async (query, { rejectWithValue, dispatch }) => {},
 );
 
 export const updateOrder = createAsyncThunk(
-  "order/updateOrder",
-  async ({ id, status }, { dispatch, rejectWithValue }) => {}
+   'order/updateOrder',
+   async ({ id, status }, { dispatch, rejectWithValue }) => {},
 );
 
 // Order slice
 const orderSlice = createSlice({
-  name: "order",
-  initialState,
-  reducers: {
-    setSelectedOrder: (state, action) => {
-      state.selectedOrder = action.payload;
-    },
-  },
-  extraReducers: (builder) => {},
+   name: 'order',
+   initialState,
+   reducers: {
+      setSelectedOrder: (state, action) => {
+         state.selectedOrder = action.payload;
+      },
+   },
+   extraReducers: (builder) => {
+      builder.addCase(createOrder.pending, (state, action) => {
+         state.loading = true;
+      });
+      builder.addCase(createOrder.fulfilled, (state, action) => {
+         state.loading = false;
+         state.error = '';
+         state.orderNum = action.payload;
+      });
+      builder.addCase(createOrder.rejected, (state, action) => {
+         state.loading = false;
+         state.error = action.payload;
+      });
+   },
 });
 
 export const { setSelectedOrder } = orderSlice.actions;
